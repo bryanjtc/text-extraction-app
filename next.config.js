e12ac80be6c0c2a4167647f96daaa363fefa4e09
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2021-11-12 23:03:21 -0500

    Added export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["upload.wikimedia.org", "firebasestorage.googleapis.com"],
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/dzilzrhfk/image/fetch/',
   },
   webpack: (config) => {
     config.experiments = { topLevelAwait: true };
